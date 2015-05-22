2c173910e079aa573dba3af689ab3ebc841e5dee
Vorschi <e1026777@student.tuwien.ac.at>
2015-05-22 17:25:49 +0200

    Code cleaning

--- test/main.cpp
+++ test/main.cpp
@@ -29,54 +29,47 @@ void convertToGrayscale(const Mat &img, Mat &imgGray){
 void computeCostVolume(const Mat &imgLeft, const Mat &imgRight, vector<Mat> &costVolumeLeft, 
 	vector<Mat> &costVolumeRight, int windowSize, int maxDisp){
 
-	Mat temp(imgLeft.rows, imgLeft.cols, CV_8UC1);
-		//=imgLeft.clone(); //TODO? -> CV_32FC1
-	//(imgLeft.rows, imgLeft.cols, CV_32FC1);
+	Mat temp(imgLeft.rows, imgLeft.cols, CV_8UC1);		//matrices for storing cost volume temporarily
+	
 
 	int cost = 0;
-	uchar costLeft=0;
-	uchar costRight=0;
 
 	int a = windowSize/2;
-	cout << a;
-	for(int i=0; i<=maxDisp; i++){	
+
+	for(int i=0; i<=maxDisp; i++){		// for-loop for disparity values
+
 		for (int x=0; x<imgLeft.cols; x++){		//for-loops for going through picture per pixel
 			for (int y=0; y<imgLeft.rows; y++){
-				for (int a=windowSize/2; a>=(-windowSize/2); a--){
+
+				for (int a=windowSize/2; a>=(-windowSize/2); a--){		//for-loops for scanning window
 					for (int  b=windowSize/2; b>=(-windowSize/2); b--){
-						if((x>i)&&(y>windowSize/2)&&((y+windowSize/2)<imgLeft.rows)&&((x+windowSize/2)<imgLeft.cols)){
+
+						if((x>i)&&(y>windowSize/2)&&((y+windowSize/2)<imgLeft.rows)&&((x+windowSize/2)<imgLeft.cols)){			//handling boundary problems
 							if(((x-a)>0)&&((x-a)<imgLeft.cols)&&((y-b)>0)&&((y-b)<imgLeft.rows)){
-								if((x-a-i)<0){
-									cost+=1000; //TODO
-								}else{
+								if((x-a-i)<0){		//setting costVolume very high for out-of-image-windows
+									cost+=1000; 
+								}else{				//calculating costVolums
 									cost+=abs(imgLeft.at<uchar>(y-b,x-a) - imgRight.at<uchar>(y-b,x-a-i));
-							//costLeft = imgLeft.at<uchar>(y-b, x-a);
-							//int tester = x-a-maxDisp;
-							//cout << tester;
-							//if(x> 10)
-							//costRight = imgRight.at<uchar>(x-a-maxDisp, y-b);
-							//cost+=abs(costLeft-costRight);
-							//cout << a;
-						//cout << b;
+							
 								}
 							}
 						}
 					}		
 				}
-				cost=cost/(windowSize*windowSize);
-				if(cost>255)cost=0;
-				//if(x==383 && y==287)
-				//	int c=0;
-				temp.at<uchar>(y,x) = cost;
+
+				cost=cost/(windowSize*windowSize);		//normalizing cost volume
+				if(cost>255){
+					cost=0;
+				}
+				temp.at<uchar>(y,x) = cost;			//write cost volume in temporary matrix and reset it
 				cost = 0;
 				
 			}
 		}
-		costVolumeLeft.push_back(temp);
+		costVolumeLeft.push_back(temp);			//writing cost Volume matrix in vector and reset it
 
 		Mat temp2(imgLeft.rows, imgLeft.cols, CV_8UC1);
 		temp = temp2;
-		//temp = Mat::ones(imgLeft.rows, imgLeft.cols, CV_8UC1);
 	}
 }
 
@@ -87,16 +80,17 @@ void selectDisparity(Mat &dispLeft, Mat &dispRight, vector<Mat> &costVolumeLeft,
 	int disperity = 0;
 	for (int x=0; x<dispLeft.cols; ++x){		//for-loops for going through picture per pixel
 		for (int y=0; y<dispLeft.rows; ++y){
-			for(int i=0; i<costVolumeLeft.size(); i++){
+			for(int i=0; i<costVolumeLeft.size(); i++){			//for-loops for different disparity values
+
 				uchar costVol = costVolumeLeft.at(i).at<uchar>(y,x);
 
-				if(costVol<displevel){
-				  displevel = costVolumeLeft.at(i).at<uchar>(y,x);
+				if(costVol<displevel){				//check if current cost volume is lower then previously lowest
+				  displevel = costVolumeLeft.at(i).at<uchar>(y,x);			//if so, set it as new lower boundary
 				  disperity = i;
 				}  
 			}
 
-			dispLeft.at<uchar>(y,x)=disperity*scaleDispFactor;
+			dispLeft.at<uchar>(y,x)=disperity*scaleDispFactor;			//set pixel in desparity map
 			disperity = 0;
 			displevel=255;
 		}
@@ -105,44 +99,30 @@ void selectDisparity(Mat &dispLeft, Mat &dispRight, vector<Mat> &costVolumeLeft,
 
 
 int main(){
-	const Mat imgLeft = imread("tsukuba_left.png", CV_LOAD_IMAGE_UNCHANGED);
+	const Mat imgLeft = imread("tsukuba_left.png", CV_LOAD_IMAGE_UNCHANGED);		//reading the two original images
 	const Mat imgRight = imread("tsukuba_right.png", CV_LOAD_IMAGE_UNCHANGED);
 	
-	//const Mat imgLeft = imread("tsukuba_left.png", CV_LOAD_IMAGE_GRAYSCALE);
-	//const Mat imgRight = imread("tsukuba_right.png", CV_LOAD_IMAGE_GRAYSCALE);
-
-	//imshow("imgLeftGray",imgLeft);
-	//imshow("imgRightGray",imgRight);
-
-	vector<Mat> costVolumeLeft;
+	vector<Mat> costVolumeLeft;			//vectors for storing the different cost volumes for each disparity
 	vector<Mat> costVolumeRight;
 	
-	int windowSize = 5;
-	int maxDisp = 15;
+	int windowSize = 7;			//scanning window size
+	int maxDisp = 15;			//definition of the maximum disparity
 
 	Mat dispLeft(imgLeft.rows, imgLeft.cols, CV_8UC1);
 	Mat dispRight(imgRight.rows, imgRight.cols, CV_8UC1);
 
-	int scaleDispFactor = 16;
+	int scaleDispFactor = 16;		//stretches the disparity levels for better visualization
 
-	Mat imgGrayLeft(imgLeft.rows, imgLeft.cols, CV_8UC1);				//single channel Mat
+	Mat imgGrayLeft(imgLeft.rows, imgLeft.cols, CV_8UC1);			//computing the grayscale images				
 	Mat imgGrayRight(imgRight.rows, imgRight.cols, CV_8UC1);
 
 	convertToGrayscale(imgLeft, imgGrayLeft);
 	convertToGrayscale(imgRight, imgGrayRight);
 
-	//imshow("imgLeftGray",imgGrayLeft);
-	//imshow("imgRightGray",imgGrayRight);
-
 	computeCostVolume(imgGrayLeft, imgGrayRight, costVolumeLeft, costVolumeRight, windowSize, maxDisp);
 
 	selectDisparity(dispLeft, dispRight, costVolumeLeft, costVolumeRight, scaleDispFactor);
 
-	
-
-	//imshow("grau", imgGrayLeft);
-	imshow("costvolume 0", costVolumeLeft.at(0));
-	imshow("costvolume 15", costVolumeLeft.at(15));
 	imshow("dispLeft",dispLeft);
 	waitKey(0);
 	return 0;
